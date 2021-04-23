6ee60f5e278fd2eacf2fb363f8932cfe1a7ef2b2
CaioAlves01 <>
2021-04-23 12:59:28 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,71 @@
+/*func div(n: Int) -> Int {
+  var c = 1
+  var quant = 0
+  while (c <= n){
+    if(n%c == 0){
+      quant += 1
+    }
+    c += 1
+  } 
+  
+  return quant
+}
+var nmr = div(n:4)
+print(nmr)*/
+
+/*func valor(n:Int) ->Bool{
+  
+ if(n>0){ return true
+ }
+ else{
+   return false
+ }
+
+}
+print(valor(n:0))*/
+
+
+/*func media(n:Double) ->String{
+ switch n {
+   case 0.0..<5.0:
+    return "D"
+   case 5.0..<7.0:
+    return "C"
+   case 7.0..<9.0:
+    return "B"
+   case 9.0..<10.1:
+    return "A"
+   
+   default:return "Valor não funcional"
+  }
+  
+}
+print(media(n:7))*/
+
+/*func arrai(a: Array<Int>){
+  print(a.reduce(0,+))
+}
+
+arrai(a:[2, 3, 26, 37, 38, 48, 60, 62, 75, 78])*/
+
+ /*func multiplicar(n: Int) {
+  let array = [2,4,6,8]
+  var newArray:[Int] = [];
+  for item in array{
+    let newElement = item*n
+    newArray.append(newElement)
+  }
+  print(newArray)
+
+}
+multiplicar(n:4)*/
+
+/*func funcaoRecursiva(n:Int) {
+  var qualquer = n
+  while(qualquer/2>1){
+    print(qualquer/2)
+      qualquer = qualquer/2
+  }
+
+}
+funcaoRecursiva(n:100)*/
